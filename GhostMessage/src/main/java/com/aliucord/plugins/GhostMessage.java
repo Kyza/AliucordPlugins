bf75c27092c059d7b07b68d7722df4feef8a4df1
Kyza <kyza@kyza.net>
2021-06-09 16:05:14 -0400

    Updated for 80.2 & fixed not disabling on guild switch

--- GhostMessage/src/main/java/com/aliucord/plugins/GhostMessage.java
+++ GhostMessage/src/main/java/com/aliucord/plugins/GhostMessage.java
@@ -4,21 +4,26 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 
+import com.aliucord.Logger;
 import com.aliucord.Utils;
 import com.aliucord.api.CommandsAPI;
 import com.aliucord.entities.Plugin;
-import com.aliucord.Logger;
-
 import com.aliucord.patcher.PinePatchFn;
+import com.aliucord.utils.RxUtils;
 import com.discord.api.commands.ApplicationCommandType;
 import com.discord.models.commands.ApplicationCommandOption;
-
+import com.discord.stores.SelectedChannelAnalyticsLocation;
+import com.discord.stores.StoreChannelsSelected;
 import com.discord.stores.StoreStream;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+
+import rx.Subscription;
 
 public class GhostMessage extends Plugin {
 	public static Logger logger = new Logger("GhostMessage");
+	public Subscription subscription;
 
 	@NonNull
 	@Override
@@ -26,7 +31,7 @@ public class GhostMessage extends Plugin {
 		Manifest manifest = new Manifest();
 		manifest.authors = new Manifest.Author[]{ new Manifest.Author("Kyza", 220584715265114113L) };
 		manifest.description = "Deletes all messages you send immediately.";
-		manifest.version = "1.0.1";
+		manifest.version = "1.0.2";
 		manifest.updateUrl = "https://raw.githubusercontent.com/Kyza/AliucordPlugins/builds/updater.json";
 		return manifest;
 	}
@@ -45,18 +50,18 @@ public class GhostMessage extends Plugin {
 			this.sets.setBool("permanent", permanent);
 
 			if (enabled) {
-				this.logger.info(context, "Enabled Ghost Message.");
-				return new CommandsAPI.CommandResult("Enabled Ghost Message.", null, false);
+				this.logger.info(Utils.appActivity, "Enabled Ghost Message.");
+				return new CommandsAPI.CommandResult(null, null, false);
 			}
-			this.logger.info(context, "Disabled Ghost Message.");
-			return new CommandsAPI.CommandResult("Disabled Ghost Message.", null, false);
+			this.logger.info(Utils.appActivity, "Disabled Ghost Message.");
+			return new CommandsAPI.CommandResult(null, null, false);
 		});
 
-		StoreStream.getGatewaySocket().getMessageCreate().I().T(Utils.createActionSubscriber(message -> {
+		this.subscription = RxUtils.subscribe(RxUtils.onBackpressureBuffer(StoreStream.getGatewaySocket().getMessageCreate()), RxUtils.createActionSubscriber(message -> {
 			if (message == null) return;
 			// It's for readability ok don't judge me.
 			if (this.sets.getBool("enabled", false)) {
-				if (message.getEditedTimestamp() == 0 && message.getAuthor().f() == StoreStream.getUsers().getMe().getId()) {
+				if (message.getEditedTimestamp() == 0 && message.getAuthor().i() == StoreStream.getUsers().getMe().getId()) {
 					if (StoreStream.getChannelsSelected().getId() == message.getChannelId()) {
 						StoreStream.getMessages().deleteMessage(message);
 					}
@@ -64,19 +69,22 @@ public class GhostMessage extends Plugin {
 			}
 		}));
 
-		// Bad. Doesn't detect switch to DMs or guilds.
-		patcher.patch("com.discord.utilities.channel.ChannelSelector$gotoChannel$1", "invoke", new Class[]{}, new PinePatchFn(callFrame -> {
-			if (!this.sets.getBool("permanent", false) && this.sets.getBool("enabled", false)) {
-				this.sets.setBool("enabled", false);
-				this.logger.info(context, "Disabled Ghost Message automatically.");
-			}
-		}));
+		patcher.patch(StoreChannelsSelected.class, "handleGuildSelected", new Class[]{}, this.onNavigate);
+		patcher.patch(StoreChannelsSelected.class, "trySelectChannel", new Class[]{long.class, long.class, Long.class, SelectedChannelAnalyticsLocation.class}, this.onNavigate);
 	}
+
+	public PinePatchFn onNavigate = new PinePatchFn(callFrame -> {
+		if (!this.sets.getBool("permanent", false) && this.sets.getBool("enabled", false)) {
+			this.sets.setBool("enabled", false);
+			this.logger.info(Utils.appActivity, "Disabled Ghost Message automatically.");
+		}
+	});
 
 	@Override
 	public void stop(Context context) {
 		patcher.unpatchAll();
 		commands.unregisterAll();
+		if (subscription != null) subscription.unsubscribe();
 	}
 
 	public static Object nullish(Object obj, Object value) {
---
+++ GhostMessage/src/main/java/com/discord/stores/SelectedChannelAnalyticsLocation.java
@@ -0,0 +1,16 @@
+package com.discord.stores;
+
+public enum SelectedChannelAnalyticsLocation {
+	EMBED("Embed"),
+	CHANNEL_LIST("Channel List"),
+	THREAD_BROWSER("Thread Browser");
+
+	private final String analyticsValue = null;
+
+	private SelectedChannelAnalyticsLocation(String str) {
+	}
+
+	public final String getAnalyticsValue() {
+		return null;
+	}
+}
